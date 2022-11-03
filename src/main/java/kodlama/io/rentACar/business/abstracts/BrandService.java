6183a9384a4b75cf7979c5aa5ff1c72f53db74b7
recepergan <68998200+recepergan@users.noreply.github.com>
2022-11-03 10:37:18 +0300

    lesson 5 update ..

--- src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -2,12 +2,17 @@ package kodlama.io.rentACar.business.abstracts;
 
 import java.util.List;
 
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 
 public interface BrandService {
 	
-	List<Brand> getAll();
-	//--
+	List<GetAllBrandsResponse> findAll();
+	//response olmasının sebebi dataları versene diyor ama biz ona listofbrand veriyoruz.
+	//return ediyoruz. bu bizim ona yanıtımız
+	//son kullanıcıdan data alıyorsam onlar request ben veriyorsam response
+	void add(CreateBrandRequest createBrandRequest);
+	
 	
 	
 
--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -1,10 +1,13 @@
 package kodlama.io.rentACar.business.concretes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
@@ -19,9 +22,33 @@ public class BrandManager implements BrandService {
 	
 
 	@Override
-	public List<Brand> getAll() {
+	public List<GetAllBrandsResponse> findAll() {
+		
+		List<Brand> brands = brandRepository.findAll();
+		List<GetAllBrandsResponse> brandsResponse = new ArrayList<GetAllBrandsResponse>();
+		
+		for(Brand brand : brands) {
+			GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+			responseItem.setId(brand.getId());
+			responseItem.setName(brand.getName());
+			
+			brandsResponse.add(responseItem);
+		}
 		// iş kuralları
-		return brandRepository.getAll();
+		return brandsResponse;
 	}
+
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		// TODO Auto-generated method stub
+		Brand brand = new Brand();
+		brand.setName(createBrandRequest.getName());
+		
+		this.brandRepository.save(brand);
+		
+	}
+
+
 
 }
